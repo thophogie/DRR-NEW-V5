48c5b6e35a3d915f0fed7aa8647ea7523c858563
drrccapioduran <drrccapioduran@gmail.com>
2025-09-02 01:27:50 +0800

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,10 @@ import { AuthProvider, useAuth } from './contexts/AuthContext';
 import { DataProvider } from './contexts/DataContext';
 import { DatabaseProvider } from './contexts/DatabaseContext';
 import { PagesProvider } from './contexts/PagesContext';
+import { PublicLayout } from './layouts/PublicLayout';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
-import { PublicLayout } from './layouts/PublicLayout';
+
 
 function App() {
   return (

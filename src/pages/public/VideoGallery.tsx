ee2458dbaab9e8a8105695f53bccbba4d83b3744
ermelpz-crypto <erwinrommelpertez@gmail.com>
2025-09-01 23:14:06 +0800

    Updated VideoGallery.tsx

--- src/pages/public/VideoGallery.tsx
+++ src/pages/public/VideoGallery.tsx
@@ -395,7 +395,7 @@ const VideoGallery: React.FC = () => {
         </div>
       )}
 
-      <style jsx>{`
+      <style jsx="true">{`
         @keyframes fadeIn {
           from { opacity: 0; }
           to { opacity: 1; }
